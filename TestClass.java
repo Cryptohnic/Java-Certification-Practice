b1f69c7f5c24f0b0ed1efc4878cfc57d43f0eb97
Brandon A <brandonwalex@gmail.com>
2023-01-12 10:17:30 -0600

    Create TestClass.java

---
+++ TestClass.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+import java.util.logging.Level;
+import java.nio.file.Paths;
+
+public class TestClass {
+    public static void main(String[] args) throws IOException{
+        Files.createDirectories(Paths.get("logs"));
+        DateTimeFormatter df=DateTimeFormatter.ofPattern("yyyyMMdd_hhmm");
+        LocalDateTime now=LocalDateTime.now();
+        String date=now.format(df);
+        String logFileName="logs/testlog-"+date+".txt";
+        FileHandler myFileHandler=new FileHandler(logFileName);
+        myFileHandler.setFormatter(new SimpleFormatter());
+        Logger ocajLogger=Logger.getLogger("OCAJ Logger");
+        ocajLogger.setLevel(Level.ALL);
+        ocajLogger.addHandler(myFileHandler);
+        ocajLogger.info("\nThis is a logged information message.");
+        myFileHandler.close();
+    }
+}
