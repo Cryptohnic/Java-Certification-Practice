448007119fc7d73642d02b57b27eb00ac0931829
Brandon A <brandonwalex@gmail.com>
2023-01-16 11:03:47 -0600

    Extra Practice

--- com/ocajexam/tutorial/TestClass.java
+++ com/ocajexam/tutorial/TestClass.java
@@ -1,20 +1,21 @@
+package com.ocajexam.tutorial;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.logging.FileHandler;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
-import java.util.logging.Level;
-import java.nio.file.Paths;
 
 public class TestClass {
     public static void main(String[] args) throws IOException{
-        Files.createDirectories(Paths.get("logs"));
+        Files.createDirectories(Paths.get("com/ocajexam/tutorial/logs"));
         DateTimeFormatter df=DateTimeFormatter.ofPattern("yyyyMMdd_hhmm");
         LocalDateTime now=LocalDateTime.now();
         String date=now.format(df);
-        String logFileName="logs/testlog-"+date+".txt";
+        String logFileName="com/ocajexam/tutorial/logs/testlog-"+date+".txt";
         FileHandler myFileHandler=new FileHandler(logFileName);
         myFileHandler.setFormatter(new SimpleFormatter());
         Logger ocajLogger=Logger.getLogger("OCAJ Logger");
