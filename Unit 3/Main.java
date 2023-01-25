455ce74673c235fd3b93ea1d120ce42b3fad771a
Brandon A <brandonwalex@gmail.com>
2023-01-25 13:47:49 -0600

    Update Main.java

--- Unit 3/Main.java
+++ Unit 3/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args){
+        /*
+            Negative numbers in binary are created with the leading bit being a 1 and then the rest being represented-
+            the same as normal except all the bits are flipped and then 1 is added. 
+            This is called 2's complement and that is the method java uses for signed binary number storage.
+         */ 
         byte a;
         a=10;
         System.out.println(a+=3);
@@ -23,5 +28,7 @@ public class Main {
         System.out.println(a>>=3);
         a=60;
         System.out.println(a>>>=3);
+        byte b=(byte)0b11111111;
+        System.out.println(b);
     }    
 }
