3351fb538752e04dfdf773e3a25de369a4a51865
Brandon A <brandonwalex@gmail.com>
2023-02-20 09:24:55 -0600

    Create Datatypes.java

---
+++ Practice/Datatypes.java
@@ -0,0 +1,13 @@
+import java.math.BigInteger;
+
+public class Datatypes{
+    public static void main(String args[]){
+        System.out.println(Byte.SIZE);
+        System.out.println(Character.SIZE);
+        System.out.println(Short.SIZE);
+        System.out.println(Integer.SIZE);
+        System.out.println(Float.SIZE);
+        System.out.println(Long.SIZE);
+        System.out.println(Double.SIZE);
+    }
+}
